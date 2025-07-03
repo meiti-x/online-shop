687d7d12b7023a24d1101f09112aa3a409e7b44c
mahdi momeni <mahdimomeni6@gmail.com>
2025-07-03 23:58:49 +0330

    ✨ Feat: add signin route

--- src/apps/auth/controllers/auth.controllers.ts
+++ src/apps/auth/controllers/auth.controllers.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { sendErrorResponse, sendResponse } from '@/pkg/response';
-import { authSignUpService } from '@auth/services/auth.service';
+import { authSignInService, authSignUpService } from '@auth/services/auth.service';
 import { BadRequestError, CustomError } from '@/pkg/error';
 import { getLogger } from '@/core/logger';
 import { createUserSchema } from './dto/createUser.dto';
+import { signInSchema } from './dto/signin.dto';
 
 export function authSignUpController(req: Request, res: Response) {
   const result = createUserSchema.safeParse(req.body);
@@ -34,9 +35,35 @@ export function authSignUpController(req: Request, res: Response) {
       sendErrorResponse({ res, err });
     });
 }
-export function authSignInController(req: Request, res: Response) {
-  console.log(req, res);
+
+export async function authSignInController(req: Request, res: Response) {
+  const result = signInSchema.safeParse(req.body);
+
+  if (!result.success) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errors: result.error.flatten().fieldErrors,
+    });
+  }
+
+  try {
+    const user = await authSignInService(req?.body.email, req?.body.password);
+
+    sendResponse({
+      res,
+      statusCode: StatusCodes.CREATED,
+      data: {
+        name: user?.name,
+        email: user?.email,
+      },
+    });
+  } catch {
+    sendErrorResponse({
+      res,
+      err: new BadRequestError('invalid username or password'),
+    });
+  }
 }
+
 export function authResetPassController(req: Request, res: Response) {
   console.log(req, res);
 }
--- src/apps/auth/services/auth.service.ts
+++ src/apps/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
-import { BadRequestError } from '@/pkg/error';
-import { hashPassword } from '@/pkg/hash';
+import { BadRequestError, NotAuthorizedError } from '@/pkg/error';
+import { comparePassword, hashPassword } from '@/pkg/hash';
 import { authRepository } from '@auth/repository/auth.repository';
 import { User } from '@prisma/client';
 
@@ -16,4 +16,21 @@ export async function authSignUpService(user: Omit<User, 'id'>): Promise<User> {
   });
 
   return newUser;
+}
+
+export async function authSignInService(email: string, password: string) {
+  const user = await authRepository.findByEmail(email);
+
+  if (!user) {
+    throw new NotAuthorizedError('Invalid credentials');
+  }
+
+  const isMatch = await comparePassword(password, user.password);
+
+  if (!isMatch) {
+    throw new NotAuthorizedError('Invalid credentials');
+  }
+
+  // Optionally: return a JWT token or user info here
+  return user;
 }
