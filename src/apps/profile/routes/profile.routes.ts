be683a114a79b37edf91a69b008be471e17eaa26
mahdi momeni <mahdimomeni6@gmail.com>
2025-07-07 11:43:13 +0330

    ✨ Feat: add profile update

--- src/apps/profile/routes/profile.routes.ts
+++ src/apps/profile/routes/profile.routes.ts
@@ -1,10 +1,11 @@
 import { Application } from 'express';
 
 import { withAuth } from '@middlewares/auth';
-import { getProfileController } from '@profile/controllers/profile.controllers';
+import { getProfileController, updateProfileController } from '@profile/controllers/profile.controllers';
 
 export function profileRoutes(app: Application) {
   app.use(withAuth);
+
   app.get('/api/v1/profile', getProfileController);
-  //   app.patch('/api/v1/profile', getProfileController);
+  app.patch('/api/v1/profile', updateProfileController);
 }
